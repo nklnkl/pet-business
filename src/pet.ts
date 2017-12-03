4eb149326659f9729febc8439dafae13fe19b9c3
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-03 18:04:48 -0500

    Refactor PetService to support promises

--- src/pet.ts
+++ src/pet.ts
@@ -12,23 +12,24 @@ class PetService {
       3: name failed validation
       4: status failed validation
   */
-  public static create (species: number, breed: number, birthDate: number, name: string, status: number) : Pet|number {
+  public static create (species: number, breed: number, birthDate: number, name: string, status: number) : Promise<Pet> {
+    return new Promise((resolve, reject) => {
+      if (!this.validateSpecies(species))
+        reject(1);
+      if (!this.validateBreed(species, breed))
+        reject(2);
+      if (!this.validateName(name))
+        reject(3);
+      if (!this.validateStatus(status))
+        reject(4);
 
-    if (!this.validateSpecies(species))
-      return 1;
-    if (!this.validateBreed(species, breed))
-      return 2;
-    if (!this.validateName(name))
-      return 3;
-    if (!this.validateStatus(status))
-      return 4;
-
-    let pet: Pet = new Pet();
-    pet.setSpecies(species);
-    pet.setBreed(breed);
-    pet.setBirthDate(birthDate);
-    pet.setName(name);
-    return pet;
+      let pet: Pet = new Pet();
+      pet.setSpecies(species);
+      pet.setBreed(breed);
+      pet.setBirthDate(birthDate);
+      pet.setName(name);
+      resolve(pet);
+    });
   }
 
   /*
@@ -37,20 +38,21 @@ class PetService {
       2: breed number is invalid
       3: name failed validation
   */
-  public static update (pet: Pet, update: Pet) : Pet|number {
+  public static update (pet: Pet, update: Pet) : Promise<Pet> {
+    return new Promise((resolve, reject) => {
+      if (update.getSpecies() == 0 || update.getSpecies() > Species.getList().length - 1)
+        reject(1);
+      if (update.getBreed() == 0 || update.getBreed() > Breeds.getList()[update.getSpecies()].length - 1)
+        reject(2);
+      if (!this.validateName(update.getName()))
+        reject(3);
 
-    if (update.getSpecies() == 0 || update.getSpecies() > Species.getList().length - 1)
-      return 1;
-    if (update.getBreed() == 0 || update.getBreed() > Breeds.getList()[update.getSpecies()].length - 1)
-      return 2;
-    if (!this.validateName(update.getName()))
-      return 3;
-
-    pet.setSpecies(update.getSpecies());
-    pet.setBreed(update.getBreed());
-    pet.setBirthDate(update.getBirthDate());
-    pet.setName(update.getName());
-    return pet;
+      pet.setSpecies(update.getSpecies());
+      pet.setBreed(update.getBreed());
+      pet.setBirthDate(update.getBirthDate());
+      pet.setName(update.getName());
+      resolve(pet);
+    });
   }
 
   public static validateName (name: string) : boolean {
