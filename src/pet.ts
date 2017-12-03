0dfc4ff881431e8e22e5fe9661c7ab7145d3fd62
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-02 21:02:18 -0500

    Add PetService.update() and validation methods

--- src/pet.ts
+++ src/pet.ts
@@ -1,16 +1,82 @@
-import {Pet} from 'pet-entity';
-import * as moment from 'moment';
-export {PetService};
+import { Pet, Breeds, Species } from 'pet-entity';
+import * as Moment from 'moment';
+import * as Validator from 'validator';
+export { PetService };
+
 class PetService {
-  public create(breed: number, species: number, birthdate: number, name: string) : Pet | number{
-      let pet: Pet = new Pet();
-      pet.setBreed(breed);
-      pet.setName(name);
 
-      if(species==0 || species==1)
-        pet.setSpecies(species);
+  /*
+    reject codes
+      1: species number is invalid
+      2: breed number is invalid
+      3: name failed validation
+      4: status failed validation
+  */
+  public static create (species: number, breed: number, birthDate: number, name: string, status: number) : Pet|number {
 
-      pet.setBirthDate(birthdate);
+    if (!this.validateSpecies(species))
+      return 1;
+    if (!this.validateBreed(species, breed))
+      return 2;
+    if (!this.validateName(name))
+      return 3;
+    if (!this.validateStatus(status))
+      return 4;
 
+    let pet: Pet = new Pet();
+    pet.setSpecies(species);
+    pet.setBreed(breed);
+    pet.setBirthDate(birthDate);
+    pet.setName(name);
+    return pet;
+  }
+
+  /*
+    reject codes
+      1: species number is invalid
+      2: breed number is invalid
+      3: name failed validation
+  */
+  public static update (pet: Pet, update: Pet) : Pet|number {
+
+    let name: string = update.getName();
+    let birthDate: any = update.getBirthDate();
+    let breed: any = update.getBreed();
+    let species: any = update.getSpecies();
+
+    if (species == 0 || species > Species.getList().length - 1)
+      return 1;
+    if (breed == 0 || breed > Breeds.getList()[species].length - 1)
+      return 2;
+    if (!this.validateName(name))
+      return 3;
+
+    pet.setSpecies(species);
+    pet.setBreed(breed);
+    pet.setBirthDate(birthDate);
+    pet.setName(name);
+    return pet;
+  }
+
+  public static validateName (name: string) : boolean {
+    if (name.length < 1) return false;
+    if (name.length > 16) return false;
+    if (!Validator.isAlpha(name)) return false;
+    return true;
+  }
+  public static validateSpecies (species: number) : boolean {
+    if (species == 0 || species > Species.getList().length - 1)
+      return false;
+    return true;
+  }
+  public static validateBreed (species: number, breed: number) : boolean {
+    if (breed == 0 || breed > Breeds.getList()[species].length - 1)
+      return false;
+    return true;
+  }
+  public static validateStatus (status: number) : boolean {
+    if (status == 0 || status > 2)
+      return false;
+    return true;
   }
 }
--- src/session.ts
+++ src/session.ts
@@ -1,9 +1,15 @@
 import {Session} from 'pet-entity';
-import * as moment from 'moment';
-export {SessionService};
+import * as Moment from 'moment';
+export { SessionService };
+
 class SessionService {
-public create(id: string) : Session{
-let session: Session = new Session();
-session.setUserId(id);
+
+  public create(id: string) : Session {
+    let session: Session = new Session();
+    session.setUserId(id);
+    session.setCreated(Moment().valueOf());
+    session.setUpdated(Moment().valueOf());
+    return session;
   }
+
 }
