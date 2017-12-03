e77a4d756211de8c85b40e5d6d6cfeb3396477ed
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-02 21:05:40 -0500

    Add module export index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { AccountService } from './account';
+import { SessionService } from './session';
+import { PetService } from './pet';
+export {
+  AccountService,
+  SessionService,
+  PetService
+}
